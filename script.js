8035db119d5913b578e11df8d844b564cbc8d27e
Jfang370 <jimmyfang23@gmail.com>
2023-09-17 18:42:23 -0400

    Revert "reusable navigation"
    This reverts commit fc87ba3d54eb51d33a792fd817230d0c1790fee0.

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-const template = document.createElement('template');
+const navLink = document.querySelectorAll('.nav_link');
+const path = window.location.pathname;
 
-template.innerHTML = `
-<header class="header">
-<div class="header_content">
-  <nav class="nav">
-	<li class="nav_item"><a class="logo"></a></li>
-	<ul class="nav_list">
-	  <li class="nav_item">
-		<a class="nav_link" href="../index.html">
-		  <span aria-hidden="false"></span>
-		  Home
-		</a>
-	  </li>
-	  <li class="nav_item">
-		<a class="nav_link" href="jimmy.html">
-		  <span aria-hidden="false"></span>
-		  Jimmy's Zoo
-		</a>
-	  </li>
-	  <li class="nav_item">
-		<a class="nav_link" href="vinson.html">
-		  <span aria-hidden="false"></span>
-		  Vinson's Zoo
-		</a>
-	  </li>
-	</ul>
-  </nav>
-</div>
-</header>
-`;
+navLink.forEach(navLink => {
+	const pathName = new URL(navLink.href).pathname;
 
-document.body.appendChild(template.content);
+	if (path === pathName) {
+		navLink.classList.add('active');
+	}
+});
