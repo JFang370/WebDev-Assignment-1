fc87ba3d54eb51d33a792fd817230d0c1790fee0
Jfang370 <jimmyfang23@gmail.com>
2023-09-17 16:56:45 -0400

    reusable navigation

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-const navLink = document.querySelectorAll('.nav_link');
-const path = window.location.pathname;
+const template = document.createElement('template');
 
-navLink.forEach(navLink => {
-	const pathName = new URL(navLink.href).pathname;
+template.innerHTML = `
+<header class="header">
+<div class="header_content">
+  <nav class="nav">
+	<li class="nav_item"><a class="logo"></a></li>
+	<ul class="nav_list">
+	  <li class="nav_item">
+		<a class="nav_link" href="../index.html">
+		  <span aria-hidden="false"></span>
+		  Home
+		</a>
+	  </li>
+	  <li class="nav_item">
+		<a class="nav_link" href="jimmy.html">
+		  <span aria-hidden="false"></span>
+		  Jimmy's Zoo
+		</a>
+	  </li>
+	  <li class="nav_item">
+		<a class="nav_link" href="vinson.html">
+		  <span aria-hidden="false"></span>
+		  Vinson's Zoo
+		</a>
+	  </li>
+	</ul>
+  </nav>
+</div>
+</header>
+`;
 
-	if (path === pathName) {
-		navLink.classList.add('active');
-	}
-});
+document.body.appendChild(template.content);
